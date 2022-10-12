58c1d683f8ecc64f6f187d09f7443dd265caee2a
helene2204 <sedova.elena.22.04@gmail.com>
2022-10-12 23:42:16 +0300

    modal and form
    add modal and input_form

--- scripts/index.js
+++ scripts/index.js
@@ -22,5 +22,36 @@ new Swiper('.goods_block',{
   navigation:{
     prevEl: '.goods_arrow_prev',
     nextEl: '.goods_arrow_next',
+  },
+  preventClicks: true,
+  a11y: false,
+})
+
+const productMoreBtn=document.querySelectorAll('.product_more')
+const modal=document.querySelector('.modal')
+const formPlaceholder=document.querySelectorAll('.form_placeholder')
+const formInput=document.querySelectorAll('.form_input')
+
+productMoreBtn.forEach((btn)=>{
+  btn.addEventListener('click', ()=> {
+  modal.classList.add('modal_open')
+})
+})
+
+modal.addEventListener('click',(event)=>{
+  if (event.target === modal){
+    modal.classList.remove('modal_open')
   }
+})
+
+formInput.forEach((input,i)=>{
+  input.addEventListener('focus',()=>{
+    formPlaceholder[i].classList.add('form_placeholder_active')
+  })
+
+  input.addEventListener('blur',()=>{
+    if (input.value === ''){
+      formPlaceholder[i].classList.remove('form_placeholder_active')
+    }
+  })
 })
